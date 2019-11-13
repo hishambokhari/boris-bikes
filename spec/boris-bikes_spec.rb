60736b58234cec045114c63209619e9a6d1429db
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-13 16:09:15 +0000

    added test to show that when releases,the bike is working. test passed.

--- spec/boris-bikes_spec.rb
+++ spec/boris-bikes_spec.rb
@@ -1,9 +1,10 @@
 require 'boris-bikes'
 describe DockingStation do
     it {is_expected.to respond_to(:release_bike)}
-
-   
-
+ 
+    it "releases a bike that is working" do
+        expect(subject.release_bike.working?).to eq(true)
+    end
 
 end
 
